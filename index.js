b1aa64ec69c44f4a6bff65d62c7a25ec7cfb8010
Xiao Zhou <1298856981@qq.com>
2020-05-03 10:36:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ let quadtree;
 
 const createAnnotationData = datapoint => ({
   note: {
-    label: datapoint.first_author_name + " " + datapoint.year,
+    label: datapoint.lab + " " + datapoint.tone,
     bgPadding: 5,
-    title: trunc(datapoint.title, 100)
   },
   x: datapoint.x,
   y: datapoint.y,
@@ -32,20 +31,26 @@ streamingLoaderWorker.onmessage = ({
       ...d,
       x: Number(d.x),
       y: Number(d.y),
-      year: Number(d.date)
-    }))
-    .filter(d => d.year);
+    }));
   data = data.concat(rows);
 
   if (finished) {
     document.getElementById("loading").style.display = "none";
 
     // compute the fill color for each datapoint
-    const languageFill = d =>
-      webglColor(languageColorScale(hashCode(d.language) % 10));
-    const yearFill = d => webglColor(yearColorScale(d.year));
+    var lab = ["a", "ai", "an", "ang", "ao", "b", "c", "ch", "d", "e", "ei", "en", "eng", "er", "f", "g", "h", "i", "ia", "ian", "iang", "iao", "ie", "ii", "iii", "in", "ing", "iong", "iou", "j", "k", "l", "m", "n", "o", "ong", "ou", "p", "q", "r", "s", "sh", "t", "u", "ua", "uai", "uan", "uang", "uei", "uen", "ueng", "uo", "v", "van", "ve", "vn", "x", "z", "zh", "sil", "sp"]
+    var lab_color = ["#781c86", "#6d1c90", "#621d99", "#571ea2", "#4e20ab", "#4a27b2","#462eb9", "#4236c1", "#403ec6", "#3f47c9", "#3f51cc", "#3e5acf","#3f63cf", "#416bce", "#4274ce", "#447ccd", "#4783c8", "#498ac4", "#4c90", "#4f97bb", "#539bb5", "#56a0ae", "#5aa5a8", "#5ea9a1", "#63ac9a", "#68af93", "#6cb28c", "#72b485", "#78b67e", "#7db877", "#83ba70", "#89bb6b", "#90bc65", "#96bd60", "#9dbe5a", "#a3be56", "#aabd52", "#b0bd4e", "#b7bd4b", "#bdbb48", "#c3ba46", "#c9b843", "#ceb541", "#d3b240", "#d8ae3e", "#dcab3c", "#dfa53b", "#e19f3a", "#e49938", "#e69237", "#e68a35", "#e68133", "#e67832", "#e56e30", "#e4632e", "#e2582c", "#e04e29", "#df4327", "#dd3726", "#dc2c24", "#db2122"]
+    //创建音素与对应颜色的序数比例尺
+    const lab_f = d3.scaleOrdinal().domain(lab).range(lab_color);
 
-    const fillColor = fc.webglFillColor().value(languageFill).data(data);
+    const tone = ["0", "1", "2", "3", "4", "6", "7", "8", "9", "sil", "XX"]
+    const tone_color = ["#781c86", "#462eb9", "#3f63cf", "#4c90c0", "#63ac9a", "#83ba70", "#aabd52", "#ceb541", "#e49938", "#e4632e", "#db2122"]
+    const tone_f = d3.scaleOrdinal().domain(tone).range(tone_color);
+
+    const labFill = d => webglColor(lab_f(d.lab));
+    const toneFill = d => webglColor(tone_f(d.tone));
+
+    const fillColor = fc.webglFillColor().value(labFill).data(data);
     pointSeries.decorate(program => fillColor(program));
 
     // wire up the fill color selector
@@ -53,7 +58,7 @@ streamingLoaderWorker.onmessage = ({
       el.addEventListener("click", () => {
         iterateElements(".controls a", el2 => el2.classList.remove("active"));
         el.classList.add("active");
-        fillColor.value(el.id === "language" ? languageFill : yearFill);
+        fillColor.value(el.id === "lab" ? labFill : toneFill);
         redraw();
       });
     });
@@ -68,13 +73,8 @@ streamingLoaderWorker.onmessage = ({
 
   redraw();
 };
-streamingLoaderWorker.postMessage("data.tsv");
+streamingLoaderWorker.postMessage("UniNet_encoder_tSNE.tsv");
 
-const languageColorScale = d3.scaleOrdinal(d3.schemeCategory10);
-const yearColorScale = d3
-  .scaleSequential()
-  .domain([1850, 2000])
-  .interpolator(d3.interpolateRdYlGn);
 const xScale = d3.scaleLinear().domain([-50, 50]);
 const yScale = d3.scaleLinear().domain([-50, 50]);
 const xScaleOriginal = xScale.copy();
